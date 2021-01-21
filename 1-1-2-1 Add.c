380c16fb1f6eb2aaacad49023b764b6a6ce718f8
SooYeong <76829114+ParkSooYeong@users.noreply.github.com>
2021-01-21 21:55:36 +0900

    Add files via upload

---
+++ 1-1-2-1 Add.c
@@ -0,0 +1,26 @@
+/* SKU CoE ITE - ParkSooYoung */
+/* Grade 1 , Semester 1 , Chapter 2 , Number 1 */
+
+// 사용자로부터 입력받은 2개의 정수의 합을 계산하여 출력
+
+#define _CRT_SECURE_NO_WARNINGS // Warining C4996 Error
+
+#include <stdio.h> // 헤더 파일 포함
+
+int main(void) // 함수의 시작
+{
+	int x; // 첫번째 정수를 저장할 변수
+	int y; // 두번째 정수를 저장할 변수
+	int sum; // 2개의 정수의 합을 저장할 변수
+
+	printf("첫번째 숫자를 입력해주세요. : "); // 입력 안내 메시지 출력
+	scanf("%d", &x); // 하나의 정수를 받아서 x에 저장
+
+	printf("두번째 숫자를 입력해주세요. : "); // 입력 안내 메시지 출력
+	scanf("%d", &y); // 하나의 정수를 받아서 y에 저장
+
+	sum = x + y; // 변수 2개를 더한다.
+	printf("두 수의 합 : %d\n", sum); // sum의 값을 10진수 형태로 출력
+
+	return 0; // 0을 외부로 반환
+}
---
+++ 1-1-2-2 Circle.c
@@ -0,0 +1,23 @@
+/* SKU CoE ITE - ParkSooYoung */
+/* Grade 1 , Semester 1 , Chapter 2 , Number 2 */
+
+// 원의 면적을 계산하는 프로그램
+
+#define _CRT_SECURE_NO_WARNINGS // Warining C4996 Error
+
+#include <stdio.h> // 헤더 파일 포함
+
+int main(void) // 함수의 시작
+{
+	float radius; // 원의 반지름
+	float area; // 면적
+
+	printf("원의 반지름을 입력해주세요. : "); // 입력 안내 메시지 출력
+	scanf("%f", &radius); // 하나의 실수를 받아서 radius에 저장
+
+	area = 3.14 * radius * radius; // 원의 면적 계산
+
+	printf("원의 면적 : %f\n", area); // area의 값을 실수 형태로 출력
+
+	return 0; // 0을 외부로 반환
+}
