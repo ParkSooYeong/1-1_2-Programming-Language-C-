42fd8640c3412c2837dfe0f7f97af122a41c7672
SooYeong <76829114+ParkSooYeong@users.noreply.github.com>
2021-01-29 19:07:09 +0900

    Update and rename 1-1-10-3 getchar.c to 1-2-10-3 getchar.c

--- 1-2-10-3 getchar.c
+++ 1-2-10-3 getchar.c
@@ -1,5 +1,5 @@
 /* SKU CoE ITE - ParkSooYoung */
-/* Grade 1 , Semester 1 , Chapter 10 , Number 3 */
+/* Grade 1 , Semester 2 , Chapter 10 , Number 3 */
 
 #define _CRT_SECURE_NO_WARNINGS // Warning C4996 Error
 
