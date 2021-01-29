a760ceab864c2adcc2827f4a5bed75a61c27588d
SooYeong <76829114+ParkSooYeong@users.noreply.github.com>
2021-01-29 19:04:52 +0900

    Update and rename 1-1-9-4 pointer_array.c to 1-2-9-4 pointer_array.c

--- 1-2-9-4 pointer_array.c
+++ 1-2-9-4 pointer_array.c
@@ -1,5 +1,5 @@
 /* SKU CoE ITE - ParkSooYoung */
-/* Grade 1 , Semester 1 , Chapter 9 , Number 4 */
+/* Grade 1 , Semester 2 , Chapter 9 , Number 4 */
 
 #define _CRT_SECURE_NO_WARNINGS //Warning C4996 Error
 
