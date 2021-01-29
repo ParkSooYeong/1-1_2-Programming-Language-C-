1bfa72f9255149865508f7dbf20ddde4700fa7c8
SooYeong <76829114+ParkSooYeong@users.noreply.github.com>
2021-01-29 19:07:21 +0900

    Update and rename 1-1-10-4 getch.c to 1-2-10-4 getch.c

--- 1-2-10-4 getch.c
+++ 1-2-10-4 getch.c
@@ -1,5 +1,5 @@
 /* SKU CoE ITE - ParkSooYoung */
-/* Grade 1 , Semester 1 , Chapter 10 , Number 4 */
+/* Grade 1 , Semester 2 , Chapter 10 , Number 4 */
 
 #define _CRT_SECURE_NO_WARNINGS // Warning C4996 Error
 
