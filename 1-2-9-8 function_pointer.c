460ef1d20c93b15391722e415b97ab66738749c8
SooYeong <76829114+ParkSooYeong@users.noreply.github.com>
2021-01-29 19:05:58 +0900

    Update and rename 1-1-9-8 function_pointer.c to 1-2-9-8 function_pointer.c

--- 1-2-9-8 function_pointer.c
+++ 1-2-9-8 function_pointer.c
@@ -1,5 +1,5 @@
 /* SKU CoE ITE - ParkSooYoung */
-/* Grade 1 , Semester 1 , Chapter 9 , Number 8 */
+/* Grade 1 , Semester 2 , Chapter 9 , Number 8 */
 
 #define _CRT_SECURE_NO_WARNINGS //Warning C4996 Error
 
