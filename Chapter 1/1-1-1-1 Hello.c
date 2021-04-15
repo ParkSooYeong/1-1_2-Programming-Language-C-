5ca56bf85d63db9f1f9678616495ec5dae63725f
SooYeong <76829114+ParkSooYeong@users.noreply.github.com>
2021-04-16 08:00:06 +0900

    Rename 1-1-1-1 Hello.c to Chapter 1/1-1-1-1 Hello.c

---
+++ Chapter 1/1-1-1-1 Hello.c
@@ -0,0 +1,10 @@
+/* SKU CoE ITE - ParkSooYoung */
+/* Grade 1 , Semester 1 , Chapter 1 , Number 1 */
+
+#include <stdio.h> // 헤더 파일 포함
+
+int main(void) // 함수의 시작
+{
+	printf("Hello World!\n"); // 실행되는 문장
+	return 0;
+} // 함수의 종료
