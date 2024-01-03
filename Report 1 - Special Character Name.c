9c95bb0f21622d31520db8487fa3e64ef0cda0de
SooYeong <76829114+ParkSooYeong@users.noreply.github.com>
2024-01-03 20:19:04 +0900

    Add files via upload

---
+++ Report 1 - Special Character Name.c
@@ -0,0 +1,21 @@
+/* SKU CoE ITE - ParkSooYoung */
+/* Grade 1 , Semester 1 , Report 1 : Special Character Name */
+
+#include <stdio.h>
+
+int main(void)
+{
+	printf ("*        * *        *        *****    * \n");
+	printf ("*        * *       * *      *     * *** \n");
+	printf ("********** ****   *   *    *       *  * \n");
+	printf ("*        * *     *     *    *     * *** \n");
+	printf ("********** *    *       *    *****    * \n");
+	printf ("                                        \n");
+	printf ("  *********    ***********      *****   \n");
+	printf ("          *         *          *     *  \n");
+	printf ("          *         *         *       * \n");
+	printf ("          *         *          *     *  \n");
+	printf ("          *         *           *****   \n");
+
+	return 0;
+}
