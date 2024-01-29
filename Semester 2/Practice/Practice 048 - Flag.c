8c972efbd441f3631ce395d20ee33e231d3b1bc7
SooYeong <76829114+ParkSooYeong@users.noreply.github.com>
2024-01-30 01:00:25 +0900

    Create Practice 048 - Flag.c

---
+++ Semester 2/Practice/Practice 048 - Flag.c
@@ -0,0 +1,13 @@
+/* SKU CoE ITE - ParkSooYoung */
+/* Grade 1 , Semester 2 , Practice 048 : Flag */
+
+#include <stdio.h> // 헤더 파일 포함
+
+int main(void) // 정수형 함수 main 시작
+{
+    printf("10진법 = %d\n", 10);
+    printf("8진법 = %#o\n", 10); // 8진법 출력
+    printf("16진법 = %#x\n", 10); // 16진법 출력
+
+    return 0; // 프로그램 종료
+}
