642b25f88022162b3acb0e3e528294bac77c1733
SooYeong <76829114+ParkSooYeong@users.noreply.github.com>
2024-01-30 00:59:17 +0900

    Create Practice 047 - printf_real.c

---
+++ Semester 2/Practice/Practice 047 - printf_real.c
@@ -0,0 +1,13 @@
+/* SKU CoE ITE - ParkSooYoung */
+/* Grade 1 , Semester 2 , Practice 047 : printf_real */
+
+#include <stdio.h> // 헤더 파일 포함
+
+int main(void) // 정수형 함수 main 시작
+{
+    printf("%f\n", 0.00123); // 0.00123
+    printf("%e\n", 0.00123);
+    printf("%E\n", 0.00123);
+
+    return 0; // 프로그램 종료
+}
