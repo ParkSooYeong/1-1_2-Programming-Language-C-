4e3d84a334af539eb15dc9c7850eefa442039f02
SooYeong <76829114+ParkSooYeong@users.noreply.github.com>
2024-02-02 02:13:19 +0900

    Create power.h

---
+++ Semester 2/Project/power.h
@@ -0,0 +1,10 @@
+/* SKU CoE ITE - ParkSooYoung */
+/* Grade 1 , Semester 2 , Project : Power - power.h */
+
+// power.c에 대한 헤더 파일
+#ifndef POWER_H
+#define POWER_H
+
+double power(int x, int y);
+
+#endif
