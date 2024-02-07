850f4698befec4a708000cbf055c3cb20d941fcf
SooYeong <76829114+ParkSooYeong@users.noreply.github.com>
2024-02-07 22:38:53 +0900

    Create Final Test 7.c

---
+++ Semester 2/Test/Final Test 7.c
@@ -0,0 +1,14 @@
+/* SKU CoE ITE - ParkSooYoung */
+/* Grade 1 , Semester 2 , Final Test 7 */
+
+#define VALUE 100
+VALUE = VALUE + 10; // NEW = VALUE + 10;
+
+#define unsigned int UNIT // #define UNIT unsigned int
+UNIT i, j, k;
+
+#define STRING "Hello!"
+printf(STRING); // printf("%s", STRING);
+
+#define IS_EVEN(x) ((x) % 2 == 0 ? "EVEN" : "ODD")
+printf(IS_EVEN(12)); // printf("%s", IS_EVEN(12));
