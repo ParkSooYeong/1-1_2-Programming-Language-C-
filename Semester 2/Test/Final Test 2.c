452f0541e69a18330a12be5eb8b8c67f77888ca2
SooYeong <76829114+ParkSooYeong@users.noreply.github.com>
2024-02-07 22:37:01 +0900

    Create Final Test 2.c

---
+++ Semester 2/Test/Final Test 2.c
@@ -0,0 +1,11 @@
+/* SKU CoE ITE - ParkSooYoung */
+/* Grade 1 , Semester 2 , Final Test 2 */
+
+struct customer
+{
+    char name[20];
+    int zip_code;
+    long mileage;
+};
+
+struct customer s; // 구조체 변수 선언
