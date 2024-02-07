c2fa1419e1c22ddfc41f202fd1f7eaa32f712f98
SooYeong <76829114+ParkSooYeong@users.noreply.github.com>
2024-02-07 22:36:37 +0900

    Create Final Test 1.c

---
+++ Semester 2/Test/Final Test 1.c
@@ -0,0 +1,9 @@
+/* SKU CoE ITE - ParkSooYoung */
+/* Grade 1 , Semester 2 , Final Test 1 */
+
+struct product
+{
+    int num; // 정수형 변수 멤버 num 정의 = 번호
+    char name[255]; // 문자형 배열 멤버 name 정의, 크기는 255 = 이름
+    int price; // 정수형 변수 멤버 price 정의 = 가격
+};
